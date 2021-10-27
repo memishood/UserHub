061c71b9c3c6aab02a8397435ebad5617db8d2ad
memishood <emre.memis.49@gmail.com>
2021-10-27 23:27:38 +0300

    create user model to show in page

---
+++ app/src/main/java/com/emrememis/android/userhub/data/model/User.java
@@ -0,0 +1,9 @@
+package com.emrememis.android.userhub.data.model;
+
+public class User {
+    public String logo, avatar_url;
+    public User(String logo, String avatar_url) {
+        this.logo = logo;
+        this.avatar_url = avatar_url;
+    }
+}
