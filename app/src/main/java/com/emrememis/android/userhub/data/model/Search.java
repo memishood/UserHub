9f7f6c979a10472876e6fdb0994bcc2dd655384a
memishood <emre.memis.49@gmail.com>
2021-10-28 17:21:08 +0300

    implement search model and service

---
+++ app/src/main/java/com/emrememis/android/userhub/data/model/Search.java
@@ -0,0 +1,10 @@
+package com.emrememis.android.userhub.data.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class Search {
+    @SerializedName("items")
+    public List<User> users;
+}
--- app/src/main/java/com/emrememis/android/userhub/data/service/GithubService.java
+++ app/src/main/java/com/emrememis/android/userhub/data/service/GithubService.java
@@ -1,10 +1,12 @@
 package com.emrememis.android.userhub.data.service;
 
+import com.emrememis.android.userhub.data.model.Search;
 import com.emrememis.android.userhub.data.model.User;
 import java.util.List;
 import io.reactivex.rxjava3.core.Single;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface GithubService {
     @GET("users")
@@ -12,4 +14,7 @@ public interface GithubService {
 
     @GET("users/{user}")
     Single<User> getUser(@Path("user") String user);
+
+    @GET("search/users")
+    Single<Search> searchUsers(@Query("q") String username);
 }
